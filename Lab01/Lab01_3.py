80e51ec05052903222cb4f1b831e504c6e18f8e7
Diego Reyna <73494895+DiegoRR00@users.noreply.github.com>
2023-04-20 10:57:47 -0600

    Uploaded task 3-4 and some minor corrections

--- Lab01/Lab01_3.py
+++ Lab01/Lab01_3.py
@@ -1,47 +1,64 @@
-import cv2
-import time
+'''
+TE3003B Integración de robótica y sistemas inteligentes
+M1. Tecnologías Emergentes
+Lab 01
+
+Equipo 3.
+    Diego Reyna Reyes               A01657387
+    Jorge Antonio Hoyo García       A01658142
+    Samantha Barrón Martínez        A01652135
+    Jorge Gerardo Iglesias Ortiz    A01653261
+20/04/2023, CDMX, México 
+
+Descripción:
+Pregunta 3. Replicar con multiples camaras usando concatenación de imágenes
+            - Usar un hilo para la captura de dos camaras y unirlas en una sola imagen
+            - Usar un hilo para el procesamiento de la imagen de las dos imágenes unidas (proponer el método)
+            - Usar un hilo para mostrar la captura de ambas camaras en una sola imagen
+Se usó como base el código generado por David Christopher Balderas Silva
+'''
 from threading import Thread
-from video_classes2 import IterPerSec
-from video_classes2 import putIterationsPerSec
-from video_classes2 import VideoGet
-from video_classes2 import VideoShow
+import threading
+from video_classes import IterPerSec
+from video_classes import putIterationsPerSec
+from video_classes import VideoGet
+from video_classes import VideoShow
+from video_classes import VideoGet_2
 
-# Dedicated thread for showing video frames with VideoShow object.
-# Main thread grabs video frames.
-def threadVideoShow(source=[0,2]):
-
+# Modifies the frames
+class thread_show:
+    def __init__(self,source):
+        #Call the modifed video getter
+        self.video_getter = VideoGet_2(source).start()
+        (self.grabbed, self.frame) = (self.video_getter.grabbed, self.video_getter.frame)
+        self.video_shower = VideoShow(self.frame).start()
+        self.itps = IterPerSec().start()
+    def start(self):
+        Thread(target=self.run, args=()).start()
+        return self
+    def run(self):
+        while True:
+            (self.grabbed, self.frame) = (self.video_getter.grabbed, self.video_getter.frame)
+            #Stop when needed
+            if not self.grabbed or self.video_shower.stopped:
+                self.video_shower.stop() 
+                self.video_getter.stop()
+                break
+            #Modify the frame
+            self.frame = putIterationsPerSec(self.frame, self.itps.itPerSec())
+            self.video_shower.frame = self.frame
+            self.itps.increment()
+        #Print final Itereations per second
+        print("Iterations per second: {}".format(self.itps.itPerSec()))
     
-    video_getter = VideoGet(source[0]).start()
-    (grabbed, frame) = (video_getter.grabbed, video_getter.frame)
-    video_shower = VideoShow(frame).start()
-    itps = IterPerSec().start()
-
-    video_getter2 = VideoGet(source[1]).start()
-    (grabbed2, frame2) = (video_getter2.grabbed, video_getter2.frame)
-    video_shower2 = VideoShow(frame2).start()
-    itps2 = IterPerSec().start()
-
-    while True:
-        (grabbed, frame) = (video_getter.grabbed, video_getter.frame)
-        (grabbed2, frame2) = (video_getter2.grabbed, video_getter2.frame)
-        if not grabbed or video_shower.stopped or not grabbed2 or video_shower2.stopped:
-            video_shower.stop() 
-            video_shower2.stop()
-            break
-
-        frame = putIterationsPerSec(frame, itps.itPerSec())
-        frame2 = putIterationsPerSec(frame2, itps2.itPerSec())
-        video_shower.frame = frame
-        video_shower2.frame = frame2
-        itps.increment()
-        itps2.increment()
-
-    print( itps.itPerSec() )
 
 def main():
-    source = [0,2] # 'video_recording.avi'
-    Thread(target=threadVideoShow, args=(source,)).start()
-    
-
+    source = [0,1]
+    #Modify thread that calls the othe ones
+    thread_show(source).start()
+    #Display active threads
+    print("Current Threads:")
+    for t in threading.enumerate():
+        print('\tThread {}'.format( t.name)) 
 if __name__ == "__main__":
     main()
--- Lab01/video_classes.py
+++ Lab01/video_classes.py
@@ -1,3 +1,23 @@
+'''
+TE3003B Integración de robótica y sistemas inteligentes
+M1. Tecnologías Emergentes
+Lab 01
+
+Equipo 3.
+    Diego Reyna Reyes               A01657387
+    Jorge Antonio Hoyo García       A01658142
+    Samantha Barrón Martínez        A01652135
+    Jorge Gerardo Iglesias Ortiz    A01653261
+20/04/2023, CDMX, México 
+
+Descripción:
+Este archivo contiene las clases necesarias para 
+hacer funcionar la solución para los 4 incisos de 
+esta actividad
+
+Se usó como base el código generado por David Christopher 
+Balderas Silva, todas las modificaciones realizadas están marcadas por el comentario # (Mod)
+'''
 from threading import Thread
 import cv2
 from datetime import datetime
@@ -54,9 +74,10 @@ class VideoGet:
 ######################################################################
 # Continuously shows a frame using a thread.
 class VideoShow:
-    def __init__(self, frame=None):
+    def __init__(self, frame=None, name = "Video"):
         self.frame = frame
         self.stopped = False
+        self.name = name # (Mod) Include a name to have an unique name in the cv2 window
 
     def start(self):
         Thread(target=self.show, args=()).start()
@@ -64,9 +85,48 @@ class VideoShow:
 
     def show(self):
         while not self.stopped:
-            cv2.imshow("Video", self.frame)
+            cv2.imshow(self.name, self.frame) # (Mod) Show the saved name
             if cv2.waitKey(1) == ord("q"):
                 self.stopped = True
 
+    def stop(self):
+        self.stopped = True
+
+######################################################################
+# (Mod) Captures video from two cameras and concatenates them
+class VideoGet_2(VideoGet):
+    def __init__(self, src=[0,2]):
+        # (Mod) Create a two of each of the elemnts on the base VideoGet
+        self.stream = cv2.VideoCapture(src[0])
+        self.stream2 = cv2.VideoCapture(src[1])
+        (self.grabbed1, self.frame1) = self.stream.read()
+        (self.grabbed2, self.frame2) = self.stream2.read()
+        self.grabbed = True
+        self.stopped = False
+        self.frame = self.frame1
+
+    def start(self):
+        Thread(target = self.get, args=()).start()
+        return self
+
+    def get(self):
+        while not self.stopped:
+            self.grabbed = self.grabbed1 and self.grabbed2 # (Mod) If one camera fails to grab, both will stop
+            if not self.grabbed:
+                self.stop()
+                self.stream.release()
+            else:
+                # (Mod) Get both images
+                (self.grabbed1, self.frame1) = self.stream.read()
+                (self.grabbed2, self.frame2) = self.stream2.read()
+                self.concat()
+    def concat(self):
+        # (Mod) Resize the bigger image so it's the same size as the smaller one
+        if self.frame1.shape[0] > self.frame2.shape[0]:
+            self.frame1 = cv2.resize(self.frame1, (int(self.frame1.shape[1] * self.frame2.shape[0] / self.frame1.shape[0]), self.frame2.shape[0]), interpolation=cv2.INTER_CUBIC)
+        else:
+            self.frame2 = cv2.resize(self.frame2, (int(self.frame2.shape[1] * self.frame1.shape[0] / self.frame2.shape[0]), self.frame1.shape[0]), interpolation=cv2.INTER_CUBIC)
+        # (Mod) Concatenate
+        self.frame = cv2.hconcat([self.frame1,self.frame2])
     def stop(self):
         self.stopped = True
